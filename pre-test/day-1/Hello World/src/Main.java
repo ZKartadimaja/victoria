31ee06c8b1156dbd8e6ad6a3c3f711961125ac5c
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-05 10:44:46 +0700

    Update Main.java

--- pre-test/day-1/Hello World/src/Main.java
+++ pre-test/day-1/Hello World/src/Main.java
@@ -1,22 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        String myName;
-        int myAge;
-        float myTemp;
-        char myGrade;
-        boolean myJava;
-        myName = "Karta";
-        myAge = 22;
-        myTemp = 36.20f;
-        myGrade = 'A';
-        myJava = false;
+//        System.out.println("Hello world!");
+//        String myName;
+//        int myAge;
+//        float myTemp;
+//        char myGrade;
+//        boolean myJava;
+//        myName = "Karta";
+//        myAge = 22;
+//        myTemp = 36.20f;
+//        myGrade = 'A';
+//        myJava = false;
+//
+//        System.out.println(myName);
+//        System.out.println(myAge);
+//        System.out.println(myTemp);
+//        System.out.println(myGrade);
+//        System.out.println(myJava);
+//
 
-        System.out.println(myName);
-        System.out.println(myAge);
-        System.out.println(myTemp);
-        System.out.println(myGrade);
-        System.out.println(myJava);
+        final double myPhi;
+        int r1;
+        int r2;
+        double L1;
+        double L2;
+        myPhi = Math.PI;
+        r1 = 10;
+        r2 = 15;
+        L1 = myPhi * (r1 * r1);
+        L2 = myPhi * (r2 * r2);
+
+        System.out.println("Luas Lingkaran dengan r = 10 adalah "+L1);
+        System.out.println("Luas Lingkaran dengan r = 15 adalah "+L2);
+
 
 
 
