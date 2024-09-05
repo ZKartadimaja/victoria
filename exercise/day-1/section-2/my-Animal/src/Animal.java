278225342e66ea143e4c2773b16fc23c851566ff
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-05 16:26:01 +0700

    inheritance-excercise

--- exercise/day-1/section-2/my-Animal/src/Animal.java
+++ exercise/day-1/section-2/my-Animal/src/Animal.java
@@ -1,5 +1,17 @@
 public class Animal {
+//    public String name;
+//    public String species;
+//
+//    public Animal(String name, String species) {
+//        this.name = name;
+//        this.species = species;
+//    }
+
     void eat() {
         System.out.println("This animal eats food.");
+    }
+
+    void makeSound() {
+        System.out.println("Yohooo");
     }
 }
