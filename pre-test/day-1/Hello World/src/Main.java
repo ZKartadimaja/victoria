c9162f9ddb03dc5244e2bcbbc0552fda17a169be
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-05 11:10:26 +0700

    Odd_Even_50

--- pre-test/day-1/Hello World/src/Main.java
+++ pre-test/day-1/Hello World/src/Main.java
@@ -19,27 +19,49 @@ public class Main {
 //        System.out.println(myJava);
 //
         // Luas Lingkaran
-        final double myPhi;
-        int r1;
-        int r2;
-        double L1;
-        double L2;
-        myPhi = Math.PI;
-        r1 = 10;
-        r2 = 15;
-        L1 = myPhi * (r1 * r1);
-        L2 = myPhi * (r2 * r2);
+//        final double myPhi;
+//        int r1;
+//        int r2;
+//        double L1;
+//        double L2;
+//        myPhi = Math.PI;
+//        r1 = 10;
+//        r2 = 15;
+//        L1 = myPhi * (r1 * r1);
+//        L2 = myPhi * (r2 * r2);
+//
+//        System.out.println("Luas Lingkaran dengan r = 10 adalah "+L1);
+//        System.out.println("Luas Lingkaran dengan r = 15 adalah "+L2);
+//
+//        //Bilangan Ganjil Genap
+//        int result = (60 % 2);
+//        System.out.println("60 % 2 = "+result);
+//        System.out.println("Genap = "+ true);
 
-        System.out.println("Luas Lingkaran dengan r = 10 adalah "+L1);
-        System.out.println("Luas Lingkaran dengan r = 15 adalah "+L2);
+        //Relational Operator
+//        int myNumber = 5;
+//        String Number = "5";
+//        boolean result_1 = (myNumber == Number);
+//        System.out.println(myNumber == Number);
 
-        //Bilangan Ganjil Genap
-        int result = (60 % 2);
-        System.out.println("60 % 2 = "+result);
-        System.out.println("Genap = "+ true);
-
-
-
+        //Odd Even Upgrade
+        int myNumber = 47;
+        System.out.println("Nomor anda adalah "+myNumber);
+        if (myNumber > 50) {
+            System.out.println("Angka lebih besar dari 50");
+            if (myNumber % 2 == 0) {
+                System.out.println(("Angka anda merupakan angka genap"));
+            } else if (myNumber % 2 == 1) {
+                System.out.println("Angka anda merupakan ganjil");
+            }
+        } else if (myNumber < 50) {
+            System.out.println("Angka lebih kecil dari 50");
+            if (myNumber % 2 == 0) {
+                System.out.println(("Angka anda merupakan angka genap"));
+            } else if (myNumber % 2 == 1) {
+                System.out.println("Angka anda merupakan ganjil");
+            }
+        }
 
 
     }
