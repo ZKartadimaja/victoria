47478ecd742842fa1b10a736772f88a1ded9fb39
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-05 10:32:52 +0700

    Primitive Data Type

--- pre-test/day-1/Hello World/src/Main.java
+++ pre-test/day-1/Hello World/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String myName;
+        int myAge;
+        float myTemp;
+        char myGrade;
+        boolean myJava;
+        myName = "Karta";
+        myAge = 22;
+        myTemp = 36.20f;
+        myGrade = 'A';
+        myJava = false;
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myTemp);
+        System.out.println(myGrade);
+        System.out.println(myJava);
+
+
+
     }
 }
