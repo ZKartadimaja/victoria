6a41d9bae2eb3c92423aec99714f7709f31a1fbd
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-06 13:40:52 +0700

    getasssetbyid

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,13 +29,13 @@ public class FirstApplication {
 //		return response;
 //	}
 
-//	@GetMapping
-//	public String sayHello(){
-//		return "Hello World";
-//	}
-
 	@GetMapping
-	public ArrayList<GetAssetResponse> getAssets(){
+	public String sayHello(){
+		return "Hello World";
+	}
+
+	@GetMapping("/assets/listAssets")
+	public ArrayList<GetAssetResponse> getAsset(){
 		ArrayList<GetAssetResponse> list = new ArrayList<>();
 		GetAssetResponse response = GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build();
 		GetAssetResponse response1 = GetAssetResponse.builder().id(2L).fullname("Taufik Adi").type("Motorcycle").amount(25000000).build();
@@ -47,9 +44,27 @@ public class FirstApplication {
 		return list;
 	}
 
-	@GetMapping("/users/{id}")
-	public ResponseEntity<Long> getUserById(Long id){
-		return new ResponseEntity<>(id, HttpStatus.OK);
+	@GetMapping("/assets/{id}")
+	public GetAssetResponse getAssetById(
+			@PathVariable Long id
+	){
+		List<GetAssetResponse> asset = new ArrayList<>();
+		asset.add(GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build());
+		asset.add(GetAssetResponse.builder().id(2L).fullname("Taufik Adi").type("Motorcycle").amount(25000000).build());
+		asset.add(GetAssetResponse.builder().id(3L).fullname("Kevin").type("Apartment").amount(250000000).build());
+		asset.add(GetAssetResponse.builder().id(4L).fullname("Joko").type("Car").amount(300000000).build());
+		asset.add(GetAssetResponse.builder().id(5L).fullname("Dodo").type("House").amount(700000000).build());
+		for (int i = 0; i < asset.size(); i++) {
+			if (asset.get(i).getId().equals(id)) {
+				return asset.get(i);
+			}
+		}
+		return null;
+		}
+
 	}
 
-}
+//	@GetMapping("/assets/{id}")
+//	public ResponseEntity<Long> getUserById(Long id){
+//		return new ResponseEntity<>(id, HttpStatus.OK);
+//	}
