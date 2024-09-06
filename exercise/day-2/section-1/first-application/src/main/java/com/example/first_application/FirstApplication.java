55f0b57ac5311fffa0d882bcfcfd70ebe3779559
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-06 15:09:39 +0700

    pageandsize

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -27,21 +27,21 @@ public class FirstApplication {
 //
 //		return response;
 //	}
-
-	@GetMapping
-	public String sayHello(){
-		return "Hello World";
-	}
-
-	@GetMapping("/assets/listAssets")
-	public ArrayList<GetAssetResponse> getAsset(){
-		ArrayList<GetAssetResponse> list = new ArrayList<>();
-		GetAssetResponse response = GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build();
-		GetAssetResponse response1 = GetAssetResponse.builder().id(2L).fullname("Taufik Adi").type("Motorcycle").amount(25000000).build();
-		list.add(response);
-		list.add(response1);
-		return list;
-	}
+//
+//	@GetMapping
+//	public String sayHello(){
+//		return "Hello World";
+//	}
+//
+//	@GetMapping("/assets/listAssets")
+//	public ArrayList<GetAssetResponse> getAsset(){
+//		ArrayList<GetAssetResponse> list = new ArrayList<>();
+//		GetAssetResponse response = GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build();
+//		GetAssetResponse response1 = GetAssetResponse.builder().id(2L).fullname("Taufik Adi").type("Motorcycle").amount(25000000).build();
+//		list.add(response);
+//		list.add(response1);
+//		return list;
+//	}
 
 //	@GetMapping("/assets/{id}")
 //	public GetAssetResponse getAssetById(
@@ -60,35 +60,67 @@ public class FirstApplication {
 //		}
 //		return null;
 //		}
+//
+//	@GetMapping("/assets/{id}")
+//	public ArrayList<GetAssetResponse> getAssetById(
+//			@PathVariable Long id
+//	){
+//		ArrayList<GetAssetResponse> list = new ArrayList<>();
+//		ArrayList<GetAssetResponse> asset;
+//        asset = new ArrayList<>();
+//        asset.add(GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build());
+//		asset.add(GetAssetResponse.builder().id(2L).fullname("Taufik Adi").type("Motorcycle").amount(25000000).build());
+//		asset.add(GetAssetResponse.builder().id(3L).fullname("Kevin").type("Apartment").amount(250000000).build());
+//		asset.add(GetAssetResponse.builder().id(4L).fullname("Joko").type("Car").amount(300000000).build());
+//		asset.add(GetAssetResponse.builder().id(5L).fullname("Dodo").type("House").amount(700000000).build());
+//        for (GetAssetResponse getAssetResponse : asset) {
+//            if (getAssetResponse.getId() >= id) {
+//                list.add(getAssetResponse);
+//            }
+//        }
+//		return list;
+//	}
+//
+//	@GetMapping("/calculator/add")
+//	public String getAdd(
+//			@RequestParam(name = "panjang") int panjang,
+//			@RequestParam(name = "lebar", required = false, defaultValue = "0") int lebar
+//	){
+//		return String.valueOf(panjang+lebar);
+//	}
+//	}
 
-	@GetMapping("/assets/{id}")
-	public ArrayList<GetAssetResponse> getAssetById(
-			@PathVariable Long id
-	){
+	@GetMapping("/assets")
+	public ArrayList<GetAssetResponse> getAssetByPageSize(
+			@RequestParam (name = "page", defaultValue = "1") int page,
+			@RequestParam (name = "size", defaultValue = "1") int size
+//			@PathVariable Long id
+	) {
 		ArrayList<GetAssetResponse> list = new ArrayList<>();
 		ArrayList<GetAssetResponse> asset;
-        asset = new ArrayList<>();
-        asset.add(GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build());
+		asset = new ArrayList<>();
+		asset.add(GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build());
 		asset.add(GetAssetResponse.builder().id(2L).fullname("Taufik Adi").type("Motorcycle").amount(25000000).build());
 		asset.add(GetAssetResponse.builder().id(3L).fullname("Kevin").type("Apartment").amount(250000000).build());
 		asset.add(GetAssetResponse.builder().id(4L).fullname("Joko").type("Car").amount(300000000).build());
 		asset.add(GetAssetResponse.builder().id(5L).fullname("Dodo").type("House").amount(700000000).build());
-        for (GetAssetResponse getAssetResponse : asset) {
-            if (getAssetResponse.getId() >= id) {
-                list.add(getAssetResponse);
-            }
-        }
-		return list;
-	}
+		asset.add(GetAssetResponse.builder().id(6L).fullname("James Bond").type("House").amount(700000000).build());
+		asset.add(GetAssetResponse.builder().id(7L).fullname("Taufik Adi").type("Motorcycle").amount(25000000).build());
+		asset.add(GetAssetResponse.builder().id(8L).fullname("Kevin").type("Apartment").amount(250000000).build());
+		asset.add(GetAssetResponse.builder().id(9L).fullname("Joko").type("Car").amount(300000000).build());
+		asset.add(GetAssetResponse.builder().id(10L).fullname("Dodo").type("House").amount(700000000).build());
 
-	@GetMapping("/calculator/add")
-	public String getAdd(
-			@RequestParam(name = "panjang") int panjang,
-			@RequestParam(name = "lebar", required = false, defaultValue = "0") int lebar
-	){
-		return String.valueOf(panjang+lebar);
+        ArrayList<GetAssetResponse> tampung = new ArrayList<>();
+        int start = (size * page - size);
+        int j = start;
+		while (j < start + size && j < asset.size()){
+			tampung.add(asset.get(j));
+			j++;
+		}
+		return tampung;
 	}
-	}
+}
+
 
 //	@GetMapping("/assets/{id}")
 //	public ResponseEntity<Long> getUserById(Long id){
