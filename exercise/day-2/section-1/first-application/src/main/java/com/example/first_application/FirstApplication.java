8a2074673aba8b58ccf4ed9c0d45cd254025b792
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-06 14:06:35 +0700

    update baru

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -3,10 +3,9 @@ package com.example.first_application;
 import com.example.first_application.response.GetAssetResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,23 +43,43 @@ public class FirstApplication {
 		return list;
 	}
 
+//	@GetMapping("/assets/{id}")
+//	public GetAssetResponse getAssetById(
+//			@PathVariable Long id
+//	){
+//		List<GetAssetResponse> asset = new ArrayList<>();
+//		asset.add(GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build());
+//		asset.add(GetAssetResponse.builder().id(2L).fullname("Taufik Adi").type("Motorcycle").amount(25000000).build());
+//		asset.add(GetAssetResponse.builder().id(3L).fullname("Kevin").type("Apartment").amount(250000000).build());
+//		asset.add(GetAssetResponse.builder().id(4L).fullname("Joko").type("Car").amount(300000000).build());
+//		asset.add(GetAssetResponse.builder().id(5L).fullname("Dodo").type("House").amount(700000000).build());
+//		for (int i = 0; i < asset.size(); i++) {
+//			if (asset.get(i).getId().equals(id)) {
+//				return asset.get(i);
+//            }
+//		}
+//		return null;
+//		}
+
 	@GetMapping("/assets/{id}")
-	public GetAssetResponse getAssetById(
+	public ArrayList<GetAssetResponse> getAssetById(
 			@PathVariable Long id
 	){
-		List<GetAssetResponse> asset = new ArrayList<>();
-		asset.add(GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build());
+		ArrayList<GetAssetResponse> list = new ArrayList<>();
+		ArrayList<GetAssetResponse> asset;
+        asset = new ArrayList<>();
+        asset.add(GetAssetResponse.builder().id(1L).fullname("James Bond").type("House").amount(700000000).build());
 		asset.add(GetAssetResponse.builder().id(2L).fullname("Taufik Adi").type("Motorcycle").amount(25000000).build());
 		asset.add(GetAssetResponse.builder().id(3L).fullname("Kevin").type("Apartment").amount(250000000).build());
 		asset.add(GetAssetResponse.builder().id(4L).fullname("Joko").type("Car").amount(300000000).build());
 		asset.add(GetAssetResponse.builder().id(5L).fullname("Dodo").type("House").amount(700000000).build());
-		for (int i = 0; i < asset.size(); i++) {
-			if (asset.get(i).getId().equals(id)) {
-				return asset.get(i);
-			}
-		}
-		return null;
-		}
+        for (GetAssetResponse getAssetResponse : asset) {
+            if (getAssetResponse.getId() >= id) {
+                list.add(getAssetResponse);
+            }
+        }
+		return list;
+	}
 
 	}
 
